5f456d90e05948d13cad5e9ab570685870844001
reshmaushakumari <reshma.ushakumari@outlook.com>
2020-06-25 20:58:01 +0000

    Parameterized queries

--- students.js
+++ students.js
@@ -7,14 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT students.id, students.name, cohort_id, cohorts.name as cohort_name
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `SELECT students.id, students.name, cohort_id, cohorts.name as cohort_name
 FROM students
 JOIN cohorts
 ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;`
+
+pool.query(queryString,values)
 .then(res => {
   res.rows.forEach(user => {
   console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort ${user.cohort_name}`);
--- teachers.js
+++ teachers.js
@@ -7,18 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-  SELECT teachers.name as teacher, cohorts.name as cohort
-  FROM assistance_requests
-  JOIN teachers ON teachers.id = teacher_id
-  JOIN students ON students.id = student_id
-  JOIN cohorts ON students.cohort_id = cohorts.id
-  WHERE cohorts.name = '${process.argv[2]}'
-  GROUP BY teachers.name, cohorts.name
-  ORDER BY teacher;
-`)
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`];
+
+const queryString = `SELECT teachers.name as teacher, cohorts.name as cohort
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+GROUP BY teachers.name, cohorts.name
+ORDER BY teacher;`
+
+pool.query(queryString,values)
 .then(res =>  {
-  //console.log(res.rows);
    res.rows.forEach(user => {
    console.log(`${user.cohort}: ${user.teacher}`);
 })})
